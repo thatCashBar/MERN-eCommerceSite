57b0deaeb039e13af9fb9b98213c69af7d855f05
Jaxon Ray Petersen <thatCashBar@gmail.com>
2021-06-29 15:51:01 -0400

    Added Models/Schema for Products

---
+++ models/productModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = mongoose.Schema(
+    {
+        userid: {
+            type: mongoose.Schema.Types.ObjectId
+        },
+        name: {
+            type: String,
+            require: true
+        },
+        comment: {
+            type: String,
+        },
+        rating: {
+            type: String,
+            require: true
+        }
+    },
+    {
+        timeStamps: true
+    }
+);
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    image: {
+        type: String,
+        require: true
+    },
+    category: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: Number,
+        require: true
+    },
+    countInStock: {
+        type: Number,
+        require: true
+    },
+    rating: {
+        type: Number,
+        require: true
+    },
+    reviews: [reviewSchema]
+});
+
+const Product = mongoose.model('products', productSchema);
+module.exports = Product;
